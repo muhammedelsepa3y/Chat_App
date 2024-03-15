3e68f18b947a8c30158e7296e5e4dc4dee0ae6ea
moels <muhammedelsepa3y@gmail.com>
2024-03-15 13:43:00 +0200

    solve some bug

--- lib/Enums/enumerations.dart
+++
@@ -1,9 +0,0 @@
-enum ResponseStatus { success, errorCatching, errorFromBack}
-enum VerifyOTPStatus{login,register,resetPassword,updatePassword,updatePhone}
-enum CheckMobileStatus{register, login, forgetPassword,updatePhone}
-
-enum ProviderComponentDo{openDetails,selectToConvert}
-enum UmrahMediaContentEnum {
-  video,
-  image,
-}
